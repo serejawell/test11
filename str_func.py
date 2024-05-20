e511ebf0be8d063eefc61154420f582d531a42e5
Sergey Petrov <sev2316@yandex.ru>
2024-05-21 00:23:45 +0300

    adding upper func

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
-"""string to upper string"""
+"""supper string func"""
 
 def upper_func(string):
     return string.upper()
+
+"""string to title string"""
+
+def title_func(string):
+    return string.title()
+
